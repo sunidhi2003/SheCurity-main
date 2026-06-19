5ecd07293bd19cb1974b386574fdd9cf7cba2593
sunidhi2003 <sunidhiagg7@gmail.com>
2026-06-19 19:38:42 +0530

    adD

--- server.js
+++ server.js
@@ -138,10 +138,10 @@ const data = response.data
 });
 
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-  app.get("*", (req, res) => res.sendFile(path.join(__dirname, "client/build", "index.html")));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/client/build")));
+//   app.get("*", (req, res) => res.sendFile(path.join(__dirname, "client/build", "index.html")));
+// }
 
 // Start server
 httpServer.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
